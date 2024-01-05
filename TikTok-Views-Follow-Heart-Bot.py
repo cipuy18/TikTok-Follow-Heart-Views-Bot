7ad0d608148880e941dde20ef80b910d09f88445
cipuy18 <155620213+cipuy18@users.noreply.github.com>
2024-01-05 17:46:04 +0700

    Update TikTok-Views-Follow-Heart-Bot.py

--- TikTok-Views-Follow-Heart-Bot.py
+++ TikTok-Views-Follow-Heart-Bot.py
@@ -101,7 +101,7 @@ def loop4():
         sleep(wait_time)
         loop4()
 
-vidUrl = "https://www.tiktok.com/@social_degradation_crazy/video/6890937108874169601" #Change with one of your tiktok videos
+vidUrl = "https://www.tiktok.com/@hayfiizh/video/7274647179946380549" #Change with one of your tiktok videos
 
 system("clear") #If you have Windows you can use cls
 tiktokbot = pyfiglet.figlet_format("NoNameoN", font="slant")
